96016839bef9708df61f1c357d16494b5b0be2d8
Sanskar Jung Rana <rsanskar31@gmail.com>
2020-05-08 15:09:36 +0545

    Second commit for this project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import Menu from'./components/MenuComponents';
 import './App.css';
+import {DISHES} from './shared/dishes';
 
-function App() {
+class App extends Component {
 
+constructor(props){
+  super(props);
+
+  this.state = {
+    dishes: DISHES
+  };
+}
+
+render(){
   return (
     <div >
      <Navbar dark color="primary">
@@ -13,10 +23,11 @@ function App() {
         <NavbarBrand href="/">Risstorant Con Fusion</NavbarBrand>
        </div>
       </Navbar>
-      <Menu />
+      <Menu dishes ={this.state.dishes} />
     </div>
   );
  
+}
 }
 
 export default App;
